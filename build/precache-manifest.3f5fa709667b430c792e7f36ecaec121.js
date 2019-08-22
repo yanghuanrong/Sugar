bf6f86a21479d34dbf7899685d17228067b4747e
yanghuanrong <1176989241@qq.com>
2019-08-22 17:07:02 +0800

    update

---
+++ build/precache-manifest.3f5fa709667b430c792e7f36ecaec121.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fedccf740038f25188e80aa5334f4147",
+    "url": "/index.html"
+  },
+  {
+    "revision": "146949fcfc3b827933fa",
+    "url": "/static/css/2.b98b06aa.chunk.css"
+  },
+  {
+    "revision": "813bae93d641f70d99b6",
+    "url": "/static/css/main.4a3f356c.chunk.css"
+  },
+  {
+    "revision": "146949fcfc3b827933fa",
+    "url": "/static/js/2.2c05597d.chunk.js"
+  },
+  {
+    "revision": "813bae93d641f70d99b6",
+    "url": "/static/js/main.ffb2d33f.chunk.js"
+  },
+  {
+    "revision": "57087445b8c3d9b59894",
+    "url": "/static/js/runtime~main.c2f40307.js"
+  }
+]);
